f6a06bc65a18d38e77a76fce2a4dd5294d70820b
Cory Fladhammer <cjfladham@hotmail.com>
2020-09-20 21:00:43 -0500

    added initial rendering tests

--- src/DefectLog/DefectLog.test.js
+++ src/DefectLog/DefectLog.test.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
+import DefectLog from './DefectLog'
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
+  ReactDOM.render(<DefectLog />, div)
   ReactDOM.unmountComponentAtNode(div)
 })
--- src/Registration/Registration.test.js
+++ src/Registration/Registration.test.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
+import Registration from './Registration'
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
+  ReactDOM.render(<Registration />, div)
   ReactDOM.unmountComponentAtNode(div)
 })
