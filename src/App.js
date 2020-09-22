06dcaf6ad8002d4e83378c20012f6e9e3f8ff0d6
Cory Fladhammer <cjfladham@hotmail.com>
2020-09-21 22:02:58 -0500

    implement routing

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import React from 'react'
+import { Route } from 'react-router-dom'
+
 import LandingPage from './LandingPage'
 import Registration from './Registration'
 import SignIn from './SignIn'
 import DhrReport from './DhrReport'
-import DefectLog from './DefectLog'
+// import DefectLog from './DefectLog'
 
 
 function App() {
     return (
         <div className="App">
-            <Registration />
-            <SignIn />
-            <LandingPage />
-            <DhrReport />
-            <DefectLog />     
+         
+            {/* <DefectLog /> */}
+            <Route exact path='/' component={SignIn} />
+
+            <Route path='/registration' component={Registration} />
+
+            <Route path='/landing' component={LandingPage} />
+
+            <Route path='/dhr-report/show/:documentId' component={DhrReport} />
+
+            {/* <Route path='/dhr-report/show-all' component={DhrReport} /> */}
         </div>
     )
 }
