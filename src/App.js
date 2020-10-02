85c1a87b7fe38e2ea4d368da4a5a4756f0dbf40d
Cory Fladhammer <cjfladham@hotmail.com>
2020-10-01 20:07:16 -0500

    updated routing

--- src/App.js
+++ src/App.js
@@ -5,23 +5,23 @@ import LandingPage from './LandingPage'
 import Registration from './Registration'
 import SignIn from './SignIn'
 import DhrReport from './DhrReport'
-// import DefectLog from './DefectLog'
+import DefectLog from './DefectLog'
 
 
 function App() {
     return (
         <div className="App">
          
-            {/* <DefectLog /> */}
-            <Route exact path='/' component={SignIn} />
+            <Route path='/defect-log' component={DefectLog} />
 
-            <Route path='/registration' component={Registration} />
+            <Route path='/dhr-report' component={DhrReport} />
 
             <Route path='/landing' component={LandingPage} />
+ 
+            <Route path='/registration' component={Registration} />
 
-            <Route path='/dhr-report/show/:documentId' component={DhrReport} />
+            <Route exact path='/' component={SignIn} />
 
-            {/* <Route path='/dhr-report/show-all' component={DhrReport} /> */}
         </div>
     )
 }
